37e2204e9b7a3e7f0b9fe97e4514fe1085f3bf23
tungarisdev <tungaris.dev@gmail.com>
2026-06-12 23:08:59 +0700

    loại bỏ cập nhật email theo form

--- src/main/java/com/example/it_iap/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/it_iap/service/impl/UserServiceImpl.java
@@ -8,8 +8,6 @@ import com.example.it_iap.dto.user.request.UpdateUserInfoRequest;
 import com.example.it_iap.dto.user.response.UserResponse;
 import com.example.it_iap.entity.User;
 import com.example.it_iap.enums.UploadFolder;
-import com.example.it_iap.dto.user.request.CreateUserRequest;
-import com.example.it_iap.dto.user.request.UpdateUserRequest;
 import com.example.it_iap.entity.enums.Role;
 import com.example.it_iap.exception.AppException;
 import com.example.it_iap.exception.ErrorCode;
@@ -114,23 +112,9 @@ public class UserServiceImpl implements UserService {
     
     public UserResponse updateInfo(UpdateUserInfoRequest request) {
         User user = getCurrentUser();
-        String email = request.getEmail();
-
-        /*
-            Nếu email khác email hiện tại thì check trong
-            csdl nếu có thì trả về lỗi email đã tồn tại
-         */
-        if (!user.getEmail().equals(email)) {
-            if (userRepository.existsByEmail(email)) {
-                throw new AppException(ErrorCode.EMAIL_EXISTED);
-            }
-        }
-        
-        user.setEmail(email);
         user.setFullName(request.getFullName());
         user.setPhoneNumber(request.getPhoneNumber());
         return buildProfileResponse(userRepository.save(user));
-
     }
 
     public String updateAvatar(MultipartFile file) {
