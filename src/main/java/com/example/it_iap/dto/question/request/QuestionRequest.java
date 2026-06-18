085216af57013f2a9e1e95a9d1e71b9b236b5390
tungarisdev <tungaris.dev@gmail.com>
2026-06-18 20:40:31 +0700

    quên import thư viện :((

--- src/main/java/com/example/it_iap/dto/question/request/QuestionRequest.java
+++ src/main/java/com/example/it_iap/dto/question/request/QuestionRequest.java
@@ -8,6 +8,8 @@ import com.example.it_iap.entity.enums.TargetLevel;
 import com.example.it_iap.entity.enums.TargetPosition;
 import com.example.it_iap.validator.annotation.EnumValue;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
