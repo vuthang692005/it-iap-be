2a67d8e983a6e8202f9907b76a061bc8e254bf58
Sơn 🥀 <trancuop8@gmail.com>
2026-06-22 21:52:31 +0700

    Sửa tí 6: Sửa model

--- src/main/java/com/example/it_iap/entity/enums/ModelType.java
+++ src/main/java/com/example/it_iap/entity/enums/ModelType.java
@@ -12,8 +12,8 @@ public enum ModelType {
     GEMINI_3_1_FLASH(ProviderType.GOOGLE, "gemini-3.1-flash"),
     GEMINI_3_1_FLASH_LITE(ProviderType.GOOGLE, "gemini-3.1-flash-lite"),
     GEMINI_3_5_FLASH(ProviderType.GOOGLE, "gemini-3.5-flash"),
-    GEMMA_4_26B(ProviderType.GOOGLE, "gemma-4-26b"),
-    GEMMA_4_31B(ProviderType.GOOGLE, "gemma-4-31b"),
+    GEMMA_4_26B_A4B_IT(ProviderType.GOOGLE, "gemma-4-26b-a4b-it"),
+    GEMMA_4_31B_IT(ProviderType.GOOGLE, "gemma-4-31b-it"),
     ;
 
     private final ProviderType providerType;
